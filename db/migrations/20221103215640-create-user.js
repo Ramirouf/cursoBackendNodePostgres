d4e0c6153ea6e142f4b54acba0615013c0da859e
Ramiro Uffelmann <ramirouf@gmail.com>
2022-11-03 19:21:59 -0300

    Finished 14th class (running migrations)

---
+++ db/migrations/20221103215640-create-user.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { query } = require('express');
+const { UserSchema, USER_TABLE } = require('./../models/user.model')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.createTable(USER_TABLE, UserSchema);
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down(queryInterface) {
+    await queryInterface.drop(USER_TABLE);
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
